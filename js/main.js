d3f671c4950197a526ef2581b496f527887d98b9
Greg1928 <gregoriosortino@protonmail.com>
2022-04-28 16:29:23 +0200

    fixed digits

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@ document.getElementById("totKm").innerHTML = totKm;
 var age = Number(prompt("How old are you?"));
 document.getElementById("age").innerHTML = age;
 var price = (totKm * 0.21);
-console.log("Initial Price:" + " " + price);
-document.getElementById("price").innerHTML = price;
+console.log("Initial Price:" + " " + price.toFixed(2));
+document.getElementById("price").innerHTML = price.toFixed(2);
 if (age < 18){
     var discount = ((price*20)/100);
     price = price - discount;
     console.log("Discount Price Under18:" + " " + price.toFixed(2));
-    document.getElementById("price").innerHTML = price;
+    document.getElementById("price").innerHTML = price.toFixed(2);
 }else if (age >= 65){
     var discount = ((price*40)/100);
     price = price - discount;
     console.log("Discount Price Over65:" + " " + price.toFixed(2));
-    document.getElementById("price").innerHTML = price;
+    document.getElementById("price").innerHTML = price.toFixed(2);
 }else{
     console.log("Full price:" + " " + price);
     

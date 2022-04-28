dee27e40b8805f86af1121a0d95512b0f08f3736
Greg1928 <gregoriosortino@protonmail.com>
2022-04-28 16:14:06 +0200

    added style

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 var totKm = Number(prompt("How many Km?"));
+document.getElementById("totKm").innerHTML = totKm;
 var age = Number(prompt("How old are you?"));
+document.getElementById("age").innerHTML = age;
 var price = (totKm * 0.21);
 console.log("Initial Price:" + " " + price);
+document.getElementById("price").innerHTML = price;
 if (age < 18){
     var discount = ((price*20)/100);
     price = price - discount;
     console.log("Discount Price Under18:" + " " + price.toFixed(2));
+    document.getElementById("price").innerHTML = price;
 }else if (age >= 65){
     var discount = ((price*40)/100);
     price = price - discount;
     console.log("Discount Price Over65:" + " " + price.toFixed(2));
+    document.getElementById("price").innerHTML = price;
 }else{
     console.log("Full price:" + " " + price);
+    
 }
